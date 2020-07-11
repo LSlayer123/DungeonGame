14a84bf70c38656cac69a0a628cb63d982e78fe2
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2020-07-11 20:19:44 +0100

    Creating a Window with an icon

--- Main.py
+++ Main.py
@@ -1,4 +1,16 @@
 import pickle
 import random
+import pygame
 
-print("hi")
+pygame.init()
+
+world = pygame.display.set_mode([800, 600])
+pygame.display.set_caption("Game")
+pygame.display.set_icon(pygame.image.load('dungeon.png'))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False

ea5e6a4c98084eb8510e219a7193d56dee2af6ac
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2020-07-25 14:53:46 +0100

    Created separate functions for different game states

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ import random
 import pygame
 from pygame.locals import *
 import os
+import sys
 
-running = True
 clock = pygame.time.Clock()
 x = 30
 y = 20
@@ -18,29 +18,41 @@ pygame.display.set_caption("Game")
 pygame.display.set_icon(pygame.image.load('dungeon.png'))
 
 
-# Main loop for the game
-while running:
-    for event in event.get():
-        if event.type == QUIT:
-            running = False
+# Main Menu Function
+def Main_Menu():
+    while True:
+        for event in event.get():
+            if event.type == QUIT:
+                pygame.QUIT()
+                sys.exit()
 
-    keystroke = pygame.key.get_pressed()
-    if keystroke[K_UP] and y > 0:
-        y -= 5
-    if keystroke[K_DOWN] and y < 720 - 60:
-        y += 5
-    if keystroke[K_LEFT] and x > 0:
-        x -= 5
-    if keystroke[K_RIGHT] and x < 1080 - 60:
-        x += 5
-    if keystroke[pygame.K_RETURN]:
-        x = 30
-        y = 20
+        keystroke = pygame.key.get_pressed()
+        if keystroke[K_UP] and y > 0:
+            y -= 5
+        if keystroke[K_DOWN] and y < 720 - 60:
+            y += 5
+        if keystroke[K_LEFT] and x > 0:
+            x -= 5
+        if keystroke[K_RIGHT] and x < 1080 - 60:
+            x += 5
+        if keystroke[pygame.K_RETURN]:
+            x = 30
+            y = 20
 
-    world.fill((255, 255, 255))
-    color = (0, 128, 255)
-    pygame.draw.rect(world, color, pygame.Rect(x, y, 60, 60))
+        world.fill((255, 255, 255))
+        color = (0, 128, 255)
+        pygame.draw.rect(world, color, pygame.Rect(x, y, 60, 60))
 
-    pygame.display.flip()
-    clock.tick(60)
+        pygame.display.flip()
+        clock.tick(60)
+
+
+# Function for standard game state
+def Game():
+    pass
+
+
+# Function for options menu
+def Options():
+    pass
 

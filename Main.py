1df50ab2b9cd1b7ceafdfe1e838cd010bc8cda01
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2020-07-26 21:25:02 +0100

    \o/ OBJECT-ORIENTED PROGRAMMING \o/

--- Main.py
+++ Main.py
@@ -11,22 +11,35 @@ world = pygame.display.set_mode([1080, 720])
 pygame.display.set_caption("Game")
 pygame.display.set_icon(pygame.image.load('dungeon.png'))
 clock = pygame.time.Clock()
-x = 30
-y = 20
+
+
+# Class for main player units
+class Unit(object):
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def Draw(self, win):
+        pygame.draw.rect(world, (0, 128, 255), pygame.Rect(self.x, self.y, 60, 60))
 
 
 # Function for redrawing world
 def Redraw_Screen():
     world.fill((255, 255, 255))
 
-    pygame.draw.rect(world, (0, 128, 255), pygame.Rect(x, y, 60, 60))
+    Box.Draw(world)
 
     pygame.display.update()
 
 
+Box = Unit(30, 20, 60, 60)
+
+
 # Main Menu Function
 def Main_Menu():
-    global x, y
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -34,17 +47,17 @@ def Main_Menu():
                 sys.exit()
 
         keystroke = pygame.key.get_pressed()
-        if keystroke[K_UP] and y > 0:
-            y -= 5
-        if keystroke[K_DOWN] and y < 720 - 60:
-            y += 5
-        if keystroke[K_LEFT] and x > 0:
-            x -= 5
-        if keystroke[K_RIGHT] and x < 1080 - 60:
-            x += 5
+        if keystroke[K_UP] and Box.y > 0:
+            Box.y -= 5
+        if keystroke[K_DOWN] and Box.y < 720 - 60:
+            Box.y += 5
+        if keystroke[K_LEFT] and Box.x > 0:
+            Box.x -= 5
+        if keystroke[K_RIGHT] and Box.x < 1080 - 60:
+            Box.x += 5
         if keystroke[pygame.K_RETURN]:
-            x = 30
-            y = 20
+            Box.x = 30
+            Box.y = 20
 
         Redraw_Screen()
         clock.tick(60)
